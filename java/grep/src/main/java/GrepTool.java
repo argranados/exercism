90c49239db18f838de63c3c5f8c71eeb12a1935f
Ciberaccion <argranados@yahoo.com>
2024-12-25 21:43:01 -0600

    modify -l validation to pass multifile test

--- java/grep/src/main/java/GrepTool.java
+++ java/grep/src/main/java/GrepTool.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class GrepTool {
@@ -35,16 +36,36 @@ class GrepTool {
                     if (flags.contains("-v")) { // invert match : print lines that do not contain the pattern
                             matchInvert = !line.contains(pattern);
                     }
+                    // if (flags.isEmpty() || flags.contains("-n" ) || flags.contains("-l" )) { // default match
+                    //     if (line.contains(pattern)) {
+                    //         matchDefault = true;
+                    //     }
+                        
+                    // }
+                    
+                    // if (flags.contains("-w")) { // match whole word
                     if (flags.isEmpty() || flags.contains("-n" ) || flags.contains("-l" )) { // default match
-                        if (line.contains(pattern)) {
+                        String wordPattern = "\\b" + Pattern.quote(pattern) + "\\b";
+                        Pattern wordRegexPattern = Pattern.compile(wordPattern);
+                        Matcher wordMatcher = wordRegexPattern.matcher(line);
+                        if (wordMatcher.find()) {
                             matchDefault = true;
                         }
-                        
-                    } 
-
-                    if (flags.contains("-l") && (matchDefault || matchCase || matchWholeLine || matchInvert)) {
+                    }
+                    
+                    if ((flags.contains("-l") || files.size() > 1 ) && (matchDefault || matchCase || matchWholeLine || matchInvert)) {
                         result.append(file);
-                        break;  
+                        if ( files.size() > 1 ) {
+                            if (flags.contains("-l")) {  // this means only flag is -l => FILES_NAMES_ONLY
+                                result.append("\n");
+                                break;
+                            } else {                       // this means there are more flags, so will append the line
+                                result.append(":");
+                            }
+                        }  else {                          // this means only one file is being processed and flag is -l => FILES_NAMES_ONLY
+                             break; 
+                        }
+
                     }
                     // these two flags cannot exlude each other need to be compared as a pair    
                     if (flags.size() == 2 && (matchWholeLine && !matchInvert)) {
