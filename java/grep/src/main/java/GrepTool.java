3987d7a08a2c8c6c238b221b593fc1cb7e30b803
Ciberaccion <argranados@yahoo.com>
2024-12-27 17:23:54 -0600

    clean up

--- java/grep/src/main/java/GrepTool.java
+++ java/grep/src/main/java/GrepTool.java
@@ -9,14 +9,12 @@ class GrepTool {
 
     String grep(String pattern, List<String> flags, List<String> files) {
         StringBuilder result = new StringBuilder();
-        Pattern regexPattern = Pattern.compile(pattern);
 
         for (String file : files) {
             try {
                 List<String> lines = Files.readAllLines(Paths.get(file));
                 int i = 0;
                 for (String line : lines) {
-                    System.out.print(line);
                     i++;
                     boolean matchCase = false;
                     boolean matchWholeLine = false;
@@ -36,14 +34,8 @@ class GrepTool {
                     if (flags.contains("-v")) { // invert match : print lines that do not contain the pattern
                             matchInvert = !line.contains(pattern);
                     }
-                    // if (flags.isEmpty() || flags.contains("-n" ) || flags.contains("-l" )) { // default match
-                    //     if (line.contains(pattern)) {
-                    //         matchDefault = true;
-                    //     }
-                        
-                    // }
                     
-                    // if (flags.contains("-w")) { // match whole word
+                    // match whole word
                     if (flags.isEmpty() || flags.contains("-n" ) || flags.contains("-l" )) { // default match
                         String wordPattern = "\\b" + Pattern.quote(pattern) + "\\b";
                         Pattern wordRegexPattern = Pattern.compile(wordPattern);
@@ -53,10 +45,12 @@ class GrepTool {
                         }
                     }
                     
+                    // -l and -x use case
                     if (flags.size() == 2 && (matchWholeLine && !matchInvert)) {
                         continue;
                     }
 
+                    // -l flag logic
                     if ((flags.contains("-l") || files.size() > 1 ) && (matchDefault || matchCase || matchWholeLine || matchInvert)) {
                         result.append(file);
                         if ( files.size() > 1 ) {
@@ -74,50 +68,21 @@ class GrepTool {
                         }
 
                     }
-                    // these two flags cannot exlude each other need to be compared as a pair    
-                    // if (flags.size() == 2 && (matchWholeLine && !matchInvert)) {
-                    //     continue;
-                    // }
 
-                    if ( matchDefault ) {
+                    if ( matchDefault | matchCase | matchWholeLine | matchInvert ) {
                         if (flags.contains("-n")) { // print line numbers
                             result.append(i).append(":");
                         }
                         result.append(line).append("\n");
                         continue;
                     } 
-                    if (matchCase ) {
-                        if (flags.contains("-n")) { // print line numbers
-                            result.append(i).append(":");
-                        }
-                        result.append(line).append("\n");
-                        continue;
-                    } 
-
-                    if (matchInvert) {   // print lines that do not contain the pattern
-                        if (flags.contains("-n")) { 
-                            result.append(i).append(":");
-                        }
-                        result.append(line).append("\n");
-                        continue;
-                    } 
-
-
-                    if (matchWholeLine) {
-                        if (flags.contains("-n")) { // print line numbers
-                            result.append(i).append(":");
-                        }
-                        result.append(line).append("\n");
-                        continue;
-                    }
                     
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        System.out.println();
-        System.out.println(result.toString()+ " .");
+ 
         return result.toString().replaceFirst("\n$", "");
     }
 
