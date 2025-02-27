a1a4fbca1cfec693a103b55d5e2a2bd0ed9e8086
Ciberaccion <argranados@yahoo.com>
2025-02-26 21:45:37 -0600

    add pigLatinTranslator

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,6 +1,81 @@
+import java.util.StringTokenizer;
+
 class PigLatinTranslator {
     public String translate(String word) {
-        throw new UnsupportedOperationException("Please implement the translate() method");
+        StringTokenizer tokenizer = new StringTokenizer(word);
+        String translated = new String();
+
+        while (tokenizer.hasMoreTokens()) {
+            translated += translateWord(tokenizer.nextToken()) + " ";
+        }
+
+        return translated.trim();
     }
+
+    String translateWord(String word) {
+        if (beginsWithVowel(word)  ) {        // RULE 1
+            word = word.concat("ay");
+        } else if (beginsWithXRorYT(word)) {    // RULE 1
+            word = word.concat("ay");            
+        } else {
+            word = beginsWithOneOrMoreConsonants(word) + "ay"; // RULE 2,3,4
+        }
+        return word;
+    }
+
+    boolean beginsWithVowel(String word) {  
+        char firstLetter = word.toLowerCase().charAt(0);
+        return (firstLetter == 'a' ||
+                firstLetter == 'e' ||
+                firstLetter == 'i' ||
+                firstLetter == 'o' ||
+                firstLetter == 'u');
+    }
+
+    String beginsWithOneOrMoreConsonants(String word) {
+        StringBuilder suffix = new StringBuilder();
+        boolean BEGIN_OF_WORD=true;     // it is true only in first iteration of while loop
+        
+        while (word.length() > 1 && !beginsWithVowel(word)) {
+            if (beginsWithQU(word)) {
+                suffix.append(word.substring(0,2));
+                word = word.substring(2);
+                System.out.println( "Begins with QU "+suffix + " " + word);
+                continue;
+            } else if (beginsWithY(word)) { // YTreatedLikeAConsonantAtTheBeginningOfAWord
+                if (BEGIN_OF_WORD) {
+                    suffix.append(word.charAt(0)); 
+                    word = word.substring(1);
+                    break;    
+                } else {    // YTreatedLikeAVowelAtTheEndOfAConsonantCluster
+                    break;
+                }
+            }
+            suffix.append(word.charAt(0)); 
+            word = word.substring(1);
+            BEGIN_OF_WORD = false;
+        }
+
+        word = word.concat(suffix.toString());
+        System.out.println(word);
+        return word;
+    }
+
+    boolean beginsWithQU(String word) {
+        if (word.length() < 2) {
+            return false;
+        }
+        return word.toLowerCase().substring(0,2).equals("qu");        
+    }
+
+    boolean beginsWithXRorYT(String word) {
+        String startChars = word.substring(0, 2);
+            return startChars.equals("xr") || startChars.equals("yt");        
+    }
+
+    boolean beginsWithY(String word) {
+        return word.toLowerCase().charAt(0) == 'y';
+    }
+    
 
 }
